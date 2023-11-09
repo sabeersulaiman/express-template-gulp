5ee0a42a1017ec6c8f6b6f452ea2ec510ad6194e
Sabeer Sulaiman <sabeersulaimanpv@gmail.com>
2023-11-09 12:09:59 +0530

    Update app.js - dummy post check

--- app.js
+++ app.js
@@ -2,8 +2,22 @@
 
 const express = require('express')
 
+function postChecker(method) {
+    if(method === "POST") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 let app = express()
 app.use(express.static("dist"))
+
+// dummy post checker middleware
+app.use("/", (req, res, next) => {
+    console.log(postChecker(req.method) ? "isPOST" : "notPost");
+    next();
+});
 
 app.listen(3000, () => {
     console.log("Connected to port 3000")
